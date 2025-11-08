ece3c8bdc17800e6e149587537fffc9b6115be09
GuillemDehonFuentes <156358165+GuillemDehonFuentes@users.noreply.github.com>
2025-11-08 02:48:15 +0100

    Add settings manager and new scenes for configuration
    Introduced SettingsManager for centralized game settings control and updated ScriptableSettings to Settings with mute functionality. Added SettingsScene and StartScene, renamed Menu.unity to MainMenuScene.unity, and updated audio mixer and input actions. Removed unused prefab from PruebaMovimientoPersonaje.unity.

--- 20aniversary/Assets/Scripts/Settings/ScriptableSettings.cs
+++ 20aniversary/Assets/Scripts/Settings/ScriptableSettings.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Settings", menuName = "Game Settings/New Game Settings")]
-public class ScriptableSettings : ScriptableObject
+public class Settings : ScriptableObject
 {
     [Header("Screen Settings")]
     [SerializeField, Tooltip("Cambia la resolucion de la pantalla del juego")] public Resolution screenResolution;
@@ -12,6 +12,7 @@ public class ScriptableSettings : ScriptableObject
     [SerializeField, Range(0,1)] public float masterVolume = 1; 
     [SerializeField, Range(0,1)] public float musicVolume = 1; 
     [SerializeField, Range(0,1)] public float sfxVolume = 1;
+    [SerializeField] public bool muteAll = false; 
 
     public enum Resolution { _3840x2160, _2560x1440, _1920x1080, _1280x720, _854x480 }
     public enum Quality {Ultra, High, Medium, Low }
